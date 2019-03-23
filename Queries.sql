dda739fe2864ca424dfd39a63f3d55451d69bfa7
Julia Cotter <35545731+juliacotter@users.noreply.github.com>
2019-03-23 11:26:14 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,3 @@
-Select all from Movie
 SELECT *
 FROM Movie
 
@@ -7,35 +6,35 @@ FROM Budget
 WHERE Production_Budget > 150000000
 
 SELECT DISTINCT Movie_Title
-FROM Multiple_Critics
+FROM Movie_Review
 WHERE Rating BETWEEN 9 AND 10;
 
 SELECT Movie_Title, COUNT(TrackNo) as Songs
-FROM multiple_songs
+FROM Multiple_Songs
 GROUP BY Movie_Title
 
-SELECT MIN(gross) AS min, MAX(gross) AS max, AVG(gross) AS avg
-FROM income
+SELECT MIN(Opening_Weekend) AS min, MAX(Opening_Weekend) AS max, AVG(Opening_Weekend) AS avg
+FROM Income
 
 SELECT  Movie_Title, COUNT(Award_Show) AS awards
-FROM award
+FROM Award
 GROUP BY Movie_Title
 HAVING awards > 2
 
 SELECT Movie_Title
-FROM budget b
+FROM Budget b
 WHERE EXISTS
 	(SELECT *
-     	 FROM income i
-	 WHERE b.Movie_Title = i.Movie_Title and i.Gross < b.Production_Budget)
+     	 FROM Income i
+	 WHERE b.Movie_Title = i.Movie_Title and i.Worldwide_Gross < b.Production_Budget)
 
 SELECT DISTINCT First_Name, Last_Name
 FROM staff s, award a, multiple_movies m
-WHERE m.Movie_Title = a.Movie_Title and s.StaffID = m.StaffID  and a.Award_Show = ‘Academy%’
+WHERE m.Movie_Title = a.Movie_Title and s.StaffID = m.StaffID  and a.Award_Show LIKE 'Academy%'
 
 SELECT c.Movie_Title, Rating
-FROM multiple_critics c, award a
-WHERE c.Movie_Title = a.Movie_Title and a.Award_Show = ‘Golden Globe’ and c.CriticID = 
+FROM Movie_Review r, Award a
+WHERE r.Movie_Title = a.Movie_Title and a.Award_Show = ‘Golden Globe’ and r.CriticID = 
 	(SELECT CriticID
-	 FROM movie_review
+	 FROM Critics
 	WHERE Critic_Name = ‘Rotten Tomatoes’)
